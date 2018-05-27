fedf5b08055bfec5911534a64996421aa5ef3561
ioannakok <ioannakok@hotmail.com>
2018-05-27 20:03:54 +0100

    Create generic drawTriangle method

--- src/Triangle.java
+++ src/Triangle.java
@@ -13,45 +13,40 @@ public class Triangle {
     }
 
     public String drawIsoscelesTriangle(int height) {
+
+        return drawTriangle(height, false);
+    }
+
+    public String drawUpsideDownIsoscelesTriangle(int height) {
+
+        return drawTriangle(height, true);
+    }
+
+    private String drawTriangle(int height, boolean isUpsideDown) {
         StringBuilder triangle = new StringBuilder();
         int maxSpaces = height - 1;
         int maxWidth = (height * 2) - 1;
 
         for(int row = 0; row < height; row++) {
+
+            int spaceEndPoint = (isUpsideDown) ? row + 1 : maxSpaces - row;
+            int spaceStartPoint = (isUpsideDown) ? 1 : 0;
+            int asteriskStartPoint = (isUpsideDown) ? row : height - row;
+            int asteriskEndPoint = (isUpsideDown) ? maxWidth - row - 1 : height + row;
+
             for(int j = 0; j <= maxWidth; j++) {
 
-                if(j < maxSpaces - row)
+                if(j >= spaceStartPoint && j < spaceEndPoint)
                     triangle.append(" ");
 
-                if(j >= height - row && j <= height + row)
+                if(j >= asteriskStartPoint && j <= asteriskEndPoint)
                     triangle.append("*");
             }
 
             if(row != maxSpaces)
                 triangle.append("\n");
         }
-        return triangle.toString();
-    }
-
-    public String drawUpsideDownIsoscelesTriangle(int height) {
-        StringBuilder triangle = new StringBuilder();
-
-
-
-        /*for(int row = 1; row < height + 1; row++) {
-
-            for(int space = 0; space < row - 1; space++)
-                triangle.append(" ");
-
-            for(int width = 0; width < (row * 2) - 1; width++)
-                triangle.append("*");
-
-            if(row != 1)
-                triangle.append("\n");
-        }*/
 
         return triangle.toString();
     }
-
-
 }
